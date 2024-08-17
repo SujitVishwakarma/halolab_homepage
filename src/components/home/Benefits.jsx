a41b81a66b4e7726d158a17d905eaf67c512cf3f
Sujit Vishwakarma <88970689+SujitVishwakarma@users.noreply.github.com>
2024-08-17 22:55:14 +0530

    Update Benefits.jsx

--- src/components/home/Benefits.jsx
+++ src/components/home/Benefits.jsx
@@ -36,7 +36,7 @@ const Benefits = () => {
     };
 
     return (
-        <div className="bg-gradient-to-r from-orange-300 to-orange-600 py-10">
+        <div className="bg-gradient-to-r from-orange-300 to-[#ba761e] py-10">
             <div className="container mx-auto px-6">
                 <h2 className="text-5xl font-bold text-white mb-10">OUR BENEFITS</h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
