f3fb91c26650b1d9dfa3b021fed8592198388478
Sujit Vishwakarma <88970689+SujitVishwakarma@users.noreply.github.com>
2024-08-17 22:54:22 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaDribbble, FaInstagram, FaBehance, FaLinkedin, FaGithub, FaNpm, FaWhatsapp } from 'react-icons/fa';
 import { FiMail } from 'react-icons/fi';
+import { FaPlusCircle } from "react-icons/fa";
+
+const categories = [
+    {
+        title: 'Design',
+        items: [
+            'Web Design',
+            'Mobile App Design',
+            'E-Commerce Web Design',
+            'Saas Website Design',
+            'Healthcare Web Design',
+            'UI/UX Design',
+            'Branding for Startups',
+            'Responsive Web Design',
+            'Landing Website Design',
+        ],
+    },
+    {
+        title: 'Development',
+        items: [
+            'Back-end Development',
+            'Front-end Development',
+            'Prismic CMS Development',
+            'Vue JS Development',
+            'CMS Development',
+            'Node JS Development',
+            'Web Development',
+            'Software Product Development',
+            'Next JS Development',
+            'Webflow Development',
+            'React JS Development',
+            'Sanity Development',
+        ],
+    },
+    {
+        title: 'Marketing',
+        items: [
+            'Search Engine Optimisation',
+            'Pitch Deck Design',
+            'Technical Audit',
+            'Social Media Design',
+            'Technical SEO',
+            'SEO Audit',
+            'Content Marketing',
+            'Conversion Rate Optimization',
+            'Local SEO',
+        ],
+    },
+];
+
+
 
 const Footer = () => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
     return (
         <footer className="bg-[#0E0E25] text-white py-12">
             <div className="container mx-auto px-4">
@@ -72,7 +129,7 @@ const Footer = () => {
                 </div>
 
                 {/* Bottom Section */}
-                <div className="mt-12 border-b border-gray-700 pt-8 ">
+                <div className="mt-12  pt-8 ">
                     <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-8 gap-4 items-center">
                         <button class="bg-gray-750 text-gray-400 border border-gray-400 border-b-4 font-medium overflow-hidden relative px-4 py-2 rounded-md hover:brightness-170 hover:border-t-4 hover:border-b active:opacity-75 outline-none duration-300 group">
                             <span class="bg-gray-400 shadow-gray-400 absolute -top-[150%] left-0 inline-flex w-80 h-[5px] rounded-md opacity-50 group-hover:top-[150%] duration-500 shadow-[0_0_10px_10px_rgba(0,0,0,0.3)]"></span>
@@ -111,6 +168,36 @@ const Footer = () => {
                         </button>
                     </div>
                 </div>
+                <div className='bg-gray-400 h-0.5 mt-6'></div>
+                {/* Show All Services Section */}
+                <div className="mt-6">
+                    <div onClick={toggleDropdown} className='flex justify-between items-center cursor-pointer'>
+                        <h1>Show all Services</h1>
+                        <span className='text-white rounded-full font-bold cursor-pointer'>
+                            <FaPlusCircle />
+                        </span>
+                    </div>
+                    
+                    {isDropdownOpen && (
+                        <div className="bg-gray-900 text-white p-8 space-y-12 mt-2 w-full">
+                            {categories.map((category, index) => (
+                                <div key={index} className="mb-12">
+                                    <h2 className="md:text-lg text-sm font-bold mb-4">{category.title}</h2>
+                                    <div className="flex flex-wrap justify-between">
+                                        {category.items.map((item, itemIndex) => (
+                                            <div key={itemIndex} className="w-full sm:w-1/2 lg:w-1/3 mb-4">
+                                                {item}
+                                            </div>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+
+                {/* Bottom Divider */}
+                <div className='bg-gray-400 h-0.5 mt-6'></div>
                 {/* Copy right part  */}
                 <div className="mt-8 flex justify-between items-center text-gray-500">
                     <p>© Sujit Vishwakarma. All rights reserved</p>
